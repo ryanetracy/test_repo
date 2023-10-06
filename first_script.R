f36ebdb456e5942d272fec611ef2569ac26618d2
tracyret <tracyret@aol.com>
2023-10-06 08:39:29 -0400

    adding cohens d

--- first_script.R
+++ first_script.R
@@ -11,8 +11,8 @@ package_loader(pckgs)
 df <- read.delim('RExam.dat', sep = '\t')
 head(df, 10)
 
-m1 <- lm(exam ~ computer + lectures + numeracy + uni, data = df)
-summary(m1)
+mod1 <- lm(exam ~ computer + lectures + numeracy + uni, data = df)
+summary(mod1)
 
 ggplot(df, aes(uni, exam)) +
   geom_point(color = 'blue',
@@ -22,3 +22,12 @@ ggplot(df, aes(uni, exam)) +
   geom_smooth(method = 'lm', color = 'red2') +
   scale_x_continuous(breaks = c(0, 1)) +
   theme_light()
+
+m1 <- mean(df$exam[df$uni == 0])
+m2 <- mean(df$exam[df$uni == 1])
+
+sd1 <- sd(df$exam[df$uni == 0])
+sd2 <- sd(df$exam[df$uni == 1])
+
+get_cohen_d(m1, m2, sd1, sd2)
+
