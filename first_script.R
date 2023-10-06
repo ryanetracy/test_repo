22c20a07eac57ba5f295d91fc1c5fa8856207f84
tracyret <tracyret@aol.com>
2023-10-06 07:28:05 -0400

    regression analyses and visualization

--- first_script.R
+++ first_script.R
@@ -11,4 +11,14 @@ package_loader(pckgs)
 df <- read.delim('RExam.dat', sep = '\t')
 head(df, 10)
 
-ghp_pdazgmgWala5vUEfs04TTg3rZH6NVD1I
+m1 <- lm(exam ~ computer + lectures + numeracy + uni, data = df)
+summary(m1)
+
+ggplot(df, aes(uni, exam)) +
+  geom_point(color = 'blue',
+             shape = 7,
+             alpha = .8,
+             position = position_jitter(.05, 0)) +
+  geom_smooth(method = 'lm', color = 'red2') +
+  scale_x_continuous(breaks = c(0, 1)) +
+  theme_light()
